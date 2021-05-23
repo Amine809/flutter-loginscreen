8735d7d7b4cce81689a31d692c1be82546436a57
Amine809 <71291261+Amine809@users.noreply.github.com>
2021-05-23 21:22:45 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:app0/Home_screen.dart';
+import 'package:app0/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
